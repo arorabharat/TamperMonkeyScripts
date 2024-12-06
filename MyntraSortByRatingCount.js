3af2dd7fb95d2df18203d4e36732b6a8e0b82d81
bharat <14126367+arorabharat@users.noreply.github.com>
2024-12-06 18:51:04 +0530

    Create MyntraSortByRatingCount.js

---
+++ MyntraSortByRatingCount.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         MyntraSortByRatingCount
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @descriptionription  This Script will enable you to sort product by rating count.
+// @author       You
+// @match        https://www.myntra.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.com
+// @grant        none
+// ==/UserScript==
+
+
+
+(function () {
+    'use strict';
+
+    let scriptExecutionDelay = 3000; // ensure the page is fully loaded after this time
+
+    setTimeout(function () {
+
+        console.log("Script is working ..");
+
+      
+
+
+    }, scriptExecutionDelay);
+})();
